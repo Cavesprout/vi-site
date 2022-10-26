1fdd82843f53f5f3ea6e73285b96b39dbec728d0
Cavesprout <thecaveisstilldark@gmail.com>
2022-10-25 23:27:27 -0400

    silly change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vi is really hecking cool!
         </a>
       </header>
     </div>
